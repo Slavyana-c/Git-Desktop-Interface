bd913481d9c9b4f753387cc9f0b2977a137c455e
Omar Alebiary <el17oa@leeds.ac.uk>
2018-11-30 17:45:04 +0000

    Updated my el17oa.cc file

--- el17oa.cc
+++ el17oa.cc
@@ -1,17 +1,51 @@
 #include "globals.h"
+#include "gitpp.h"
 
 #include <QLabel>
+#include <QHBoxLayout>
+#include <QFormLayout>
+#include <QDesktopServices>
+#include <QUrl>
+#include <QLineEdit>
+
 
 namespace{
 
-class HelloWorldLabel : public QLabel{
+class HelloWorldLabel : public QWidget{
+
+
 public:
-	HelloWorldLabel() : QLabel(){
-		setText("These are your commits:");
+	HelloWorldLabel() : QWidget(){
+		//QHBoxLayout* h=new QHBoxLayout;
+		QLabel *label = new QLabel(this);
+    QHBoxLayout *layout = new QHBoxLayout();
+		QFormLayout *layout1 = new QFormLayout;
+    //label->setText("<u><b>These are your commits:</b></u>\n\n\n\n");
+
+    GITPP::REPO r;
+		for (auto i : r.commits()){
+
+			//name->setText(QString::fromStdString(i.name()));
+			//h->addWidget(new QLabel(i.value()));
+
+			QString name = QString::fromStdString(i.signature().name());
+			QString message = QString::fromStdString(i.message());
+			//QString time1 = QString::fromStdString(i.time_seconds());
+			//QString value = QString::fromStdString(i.name());
+			layout1->addRow(new QLabel(name),new QLabel(message)/*,new QLabel(time1)*/);
+		}
+		setLayout(layout1);
+		// for(auto i : r.commits()){
+		//
+		// 	h->addWidget( << i << " " << i.signature().name() <</* i.message()<<*/"\n");
+		// }
+		// setLayout(h);
 	}
 };
 
+
+
+
 INSTALL_TAB(HelloWorldLabel, "List Commits");
-//INSTALL_TAB(HelloWorldLabel, "config");
 
 }
