c488fc50d648e80a5f9a4c8c2e52c4c9fdddc746
Slavyana Chervenkondeva [sc17sdc] <sc17sdc@comp-pc6067.leeds.ac.uk>
2018-11-30 16:54:19 +0000

    Configurations are listed in the Configure Repository tab

--- sc17sdc.cc
+++ sc17sdc.cc
@@ -1,24 +1,44 @@
 #include "globals.h"
 #include "gitpp.h"
+#include "window.h"
 
 #include <QLabel>
+#include <QString>
 #include <QHBoxLayout>
+#include <QStatusBar>
+#include <QFormLayout>
+
 
 namespace{
 
 class HelloWorldLabel : public QWidget{
 public:
 	HelloWorldLabel() : QWidget(){
-		QHBoxLayout* h=new QHBoxLayout;
 
-		h->addWidget(new QLabel("left"));
-		h->addWidget(new QLabel("right"));
+		// QHBoxLayout* h=new QHBoxLayout;
+    QFormLayout *layout = new QFormLayout;
+
+    GITPP::REPO r;
+    for (auto i : r.config())
+    {
+      //name->setText(QString::fromStdString(i.name()));
+      //h->addWidget(new QLabel(i.value()));
+    
+      QString name = QString::fromStdString(i.name());
+      QString value = QString::fromStdString(i.value());
+      layout->addRow(new QLabel(name), new QLabel(value));
+    }
+
 		//h->setText("Config repository:");
-		setLayout(h);
+		//setLayout(h);
 
-		QLabel *label = new QLabel(this);
-    QHBoxLayout *layout = new QHBoxLayout();
-    label->setText("Config repo");
+  //  layout->addRow(new QLabel(tr("Line 1:")), new QLabel(tr("Line 1:")));
+    setLayout(layout);
+
+		// QLabel *label = new QLabel(this);
+    // QHBoxLayout *layout = new QHBoxLayout();
+    // label->setText("Config repo");
+    //HelloWindow::statusBar()->showMessage("Program developed and owned by ...");
 
 	}
 };
