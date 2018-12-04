1a1005bec1ca5f5e2ef971950c506627b8204e2a
Xiangdi Chai <sc17xc@leeds.ac.uk>
2018-12-04 23:36:38 +0000

    connection between selection and path created, but cant trigger actions in other tabs

--- sc17xc.cc
+++ sc17xc.cc
@@ -12,15 +12,19 @@
 #include <QAbstractButton>
 #include <QButtonGroup>
 #include <QRadioButton>
+#include <QObject>
+#include <QSignalMapper>
 
 namespace{
 
 class HelloWorldLabel : public QWidget{
+	Q_OBJECT
 public:
 	HelloWorldLabel() : QWidget(){
 		QVBoxLayout *layout = new QVBoxLayout(this);
 		GITPP::REPO r;
     int noOfBranches = 0;
+		QSignalMapper *mapper = new QSignalMapper();
 
 		QButtonGroup* group = new QButtonGroup();
 		group->setExclusive(true);
@@ -32,15 +36,33 @@ public:
 			QRadioButton *button = new QRadioButton(name);
 			layout->addWidget(button);
 			group->addButton(button,noOfBranches);
-			// connect(button,SIGNAL(troggled(bool),this,SLOT)
+			connect(button, SIGNAL(clicked()), mapper, SLOT(map()));
+			mapper->setMapping(button, button->text());
+			//std::cout << "hi\n";
+			//std::cout << button->text().toUtf8().constData();
+			connect(mapper, SIGNAL(mapped(QString)), this, SLOT(changePath(QString)));
+			//connect(button,&SIGNAL(clicked()),SLOT(changePath(button)));
  			if (name == "master"){
 				group->button(noOfBranches)->setChecked(true);
  			}
-
  		}
-
 		setLayout(layout);
 	}
+private slots:
+	void  changePath(const QString& name){
+		GITPP::REPO r;
+		//std::string fileName;
+		//QString name = button->objectName();
+		std::string utf8_text = name.toUtf8().constData();
+		//std::cout<<utf8_text;
+		//r.checkout( utf8_text);
+		std::string fileName = utf8_text;
+		//sINSTALL_TAB(HelloWorldLabel, "List Commits");
+	}
+
 };
+
 INSTALL_TAB(HelloWorldLabel, "Branches");
 }
+
+#include "sc17xc.moc"
