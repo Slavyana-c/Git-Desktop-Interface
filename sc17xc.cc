c11a0af4f93a802bf23498f75ce41a11b242dd7c
Xiangdi Chai <sc17xc@leeds.ac.uk>
2018-12-09 15:58:38 +0000

    message display after swithing branch

--- sc17xc.cc
+++ sc17xc.cc
@@ -3,17 +3,14 @@
 #include <string>
 #include <gitpp.h>
 #include <QLabel>
-#include <QHBoxLayout>
-#include <QCheckBox>
 #include <QString>
-#include <QStatusBar>
 #include <QFormLayout>
 #include <QVBoxLayout>
-#include <QAbstractButton>
 #include <QButtonGroup>
 #include <QRadioButton>
 #include <QObject>
 #include <QSignalMapper>
+#include <QMessageBox>
 
 namespace{
 
@@ -38,26 +35,18 @@ public:
 			group->addButton(button,noOfBranches);
 			connect(button, SIGNAL(clicked()), mapper, SLOT(map()));
 			mapper->setMapping(button, button->text());
-			//std::cout << "hi\n";
-			//std::cout << button->text().toUtf8().constData();
-			connect(mapper, SIGNAL(mapped(QString)), this, SLOT(changePath(QString)));
-			//connect(button,&SIGNAL(clicked()),SLOT(changePath(button)));
  			if (name == "master"){
 				group->button(noOfBranches)->setChecked(true);
  			}
+
  		}
+		connect(mapper, SIGNAL(mapped(QString)), this, SLOT(changePath(QString)));
+
 		setLayout(layout);
 	}
 private slots:
 	void  changePath(const QString& name){
-		GITPP::REPO r;
-		//std::string fileName;
-		//QString name = button->objectName();
-		std::string utf8_text = name.toUtf8().constData();
-		//std::cout<<utf8_text;
-		//r.checkout( utf8_text);
-		std::string fileName = utf8_text;
-		//sINSTALL_TAB(HelloWorldLabel, "List Commits");
+		QMessageBox::information(this,"successful","successfully switched to "+ name);
 	}
 
 };
