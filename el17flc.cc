629be19ce2ae912575f5c8167f319d9d255fb5d8
Frazer Leslie Clews <el17flc@leeds.ac.uk>
2018-12-09 15:10:35 +0000

    added layout

--- el17flc.cc
+++ el17flc.cc
@@ -1,27 +1,82 @@
 #include "globals.h"
+#include "tabs.h"
+#include "window.h"
 
+#include <QDialog>
 #include <QLabel>
+#include <QSpinBox>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QCheckBox>
+#include <QRadioButton>
+#include <QPalette>
 
 namespace{
 
 class HelloWorldLabel : public QWidget{
-public:
-	HelloWorldLabel() : QWidget(){
-		QHBoxLayout* h=new QHBoxLayout;
+	public:
+		HelloWorldLabel() : QWidget(){
+			QHBoxLayout* h = new QHBoxLayout;
+			QHBoxLayout* fontlabel = new QHBoxLayout;
+			QVBoxLayout* v1 = new QVBoxLayout;
+			QVBoxLayout* v2 = new QVBoxLayout;
 
-		h->addWidget(new QLabel("left"));
-		h->addWidget(new QLabel("right"));
-		//h->setText("Config repository:");
-		setLayout(h);
+			QCheckBox* darktheme = new QCheckBox("- Dark Theme", this);
+			connect(darktheme, SIGNAL(triggered()), pQMainWndow, SLOT(dark()));
 
-		QLabel *label = new QLabel(this);
-    QHBoxLayout *layout = new QHBoxLayout();
-    label->setText("Config repo");
+			QRadioButton* normal = new QRadioButton("Normal", this);
+			QRadioButton* colourful = new QRadioButton("Colourful", this);
+			QRadioButton* colourblind = new QRadioButton("Colour Blind", this);
 
-	}
+			QCheckBox* activetab = new QCheckBox("- Active Tabs Bold", this);
+			QSpinBox* fontsize = new QSpinBox();
+			fontsize->setRange(5, 50);
+			fontsize->setValue(14);
+
+			v1->addWidget(darktheme);
+			v1->addWidget(new QLabel("Themes"));
+			v1->addWidget(normal);
+			v1->addWidget(colourful);
+			v1->addWidget(colourblind);
+
+			activetab->setChecked(1);
+			v2->addWidget(activetab);
+			fontlabel->addWidget(fontsize);
+			fontlabel->addWidget(new QLabel("- Font Size"));
+			v2->addLayout(fontlabel);
+
+			h->addLayout(v1);
+			h->addLayout(v2);
+
+			setLayout(h);
+
+			//IF DARK THEME IS ACTIVE
+			//https://wiki.qt.io/How_to_Change_the_Background_Color_of_QWidget
+			/*if(darktheme -> isChecked()){
+				//TODO SET INACTIVE TABS TO #212121
+				//HelloWindow(_mainWidget)->setStyleSheet("background-color: #424242");
+			} else {
+			//TODO SET INACTIVE TABS TO #F5F5F5
+				//HelloWindow(_mainWidget)->setStyleSheet("background-color: #FAFAFA");
+			}*/
+
+			//IF THEME PROFILES
+			if(normal->isChecked()){
+				//HelloWindow(_mainWidget)->setStyleSheet("background-color: red;");
+			} else if(colourful->isChecked()){
+
+			} else {
+
+			}
+
+			//IF ACTIVETAB IS CHECKED
+			if(activetab->isChecked()){
+
+			}
+		}
 };
 
-INSTALL_TAB(HelloWorldLabel, "Settings");
 
+
+INSTALL_TAB(HelloWorldLabel, "Settings");
 }
